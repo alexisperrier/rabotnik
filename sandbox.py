c8a5428da03547e1b63791d6348e9cdbc6c4eec0
Alexis Perrier <alexis.perrier@pm.me>
2020-07-29 07:52:11 +0200

    sandbox

--- sandbox.py
+++ sandbox.py
@@ -18,24 +18,25 @@ from pathlib import Path
 
 if __name__ == '__main__':
 
-    flowname  = 'video_stats'
+    flowname  = 'complete_videos'
     classname = 'Flow'+ ''.join(word.title() for word in flowname.split('_'))
     klass = globals()[classname]
 
-    params = {'flowtag' : True, 'mode' : 'local', 'counting' : True, 'max_items': 2}
+    params = {'flowtag' : False, 'mode' : 'local', 'counting' : False, 'max_items': 2}
     op = klass(**params)
+    op.get_items()
 
 
-    for operation in op.operations:
-        start_time = datetime.datetime.now()
-        print("--",operation)
-        if op.ok:
-            getattr(op, operation)()
-        else:
-            print("nok", getattr(op, 'reason', ''), getattr(op, 'status_code', ''))
-            break;
-        delta_time = (datetime.datetime.now() - start_time).seconds
-        print("--",operation,f"execution time {delta_time}s")
+    # for operation in op.operations:
+    #     start_time = datetime.datetime.now()
+    #     print("--",operation)
+    #     if op.ok:
+    #         getattr(op, operation)()
+    #     else:
+    #         print("nok", getattr(op, 'reason', ''), getattr(op, 'status_code', ''))
+    #         break;
+    #     delta_time = (datetime.datetime.now() - start_time).seconds
+    #     print("--",operation,f"execution time {delta_time}s")
 
     op.execution_time()
 
--- sql/sandbox.sql
+++ sql/sandbox.sql
@@ -1,13 +1,11 @@
-select count(*) as n, pch.status
+select v.video_id, v.published_at, p.status, pch.status, pch.id
 from video v
-join pipeline p on p.video_id = v.video_id
-join pipeline pch on p.channel_id = v.channel_id
-left join flow as fl on fl.video_id = v.video_id and fl.flowname = 'video_stats'
-left join video_stat vs on ( (vs.video_id = v.video_id) and (vs.viewed_at > '2020-07-27'))
-where v.pubdate in ('2020-07-27','2020-07-26','2020-07-25','2020-07-24','2020-07-23','2020-07-22','2020-07-21','2020-07-14','2020-07-07','2020-06-30','2020-05-29')
-   -- and pch.status = 'active'
-   --and v.category_id != 20
-   and fl.id is null
-   and vs.id is null
-   and p.status = 'active'
-   group by pch.status;
+    join pipeline p on p.video_id = v.video_id
+    left join pipeline pch on pch.channel_id = v.channel_id
+    left join flow as fl on (fl.video_id = v.video_id and fl.flowname = 'complete_videos')
+where  v.video_id in (select video_id from pipeline where status = 'incomplete' and video_id is not null order by id desc limit 500)
+and p.status = 'incomplete'
+and (pch.status = 'active' or pch.id is null)
+and fl.id is null
+limit 2
+offset 2;
