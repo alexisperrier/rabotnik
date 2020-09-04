392ef3559f7adf4ae33d7d4b8c8c2b94c2a94ee0
Alexis Perrier <alexis.perrier@pm.me>
2020-09-04 16:07:05 +0200

    sandbox

--- sandbox.py
+++ sandbox.py
@@ -18,28 +18,34 @@ from pathlib import Path
 
 if __name__ == '__main__':
 
-    flowname  = 'video_stats'
+    flowname  = 'video_comments'
     classname = 'Flow'+ ''.join(word.title() for word in flowname.split('_'))
     klass = globals()[classname]
     job.config['offset_factor'] = 0
 
-    params = {'flowtag' : False, 'mode' : 'local', 'counting' : True, 'max_items': 3}
+    params = {'flowtag' : False, 'mode' : 'local', 'counting' : True, 'max_items': 10}
     op = klass(**params)
     # op.get_items()
-    # op.parse()
+    # op.freeze()
+    # op.query_api()
+    # op.decode()
+    # op.ingest()
+
+
+    # op.query_api()
     # op.ingest()
     # op.ingest_collections()
 
-    # for operation in op.operations:
-    #     start_time = datetime.datetime.now()
-    #     print("== --",operation)
-    #     if op.ok:
-    #         getattr(op, operation)()
-    #     else:
-    #         print("nok", getattr(op, 'reason', ''), getattr(op, 'status_code', ''))
-    #         break;
-    #     delta_time = (datetime.datetime.now() - start_time).seconds
-    #     print("--",operation,f"execution time {delta_time}s")
+    for operation in op.operations:
+        start_time = datetime.datetime.now()
+        print("== --",operation)
+        if op.ok:
+            getattr(op, operation)()
+        else:
+            print("nok", getattr(op, 'reason', ''), getattr(op, 'status_code', ''))
+            break;
+        delta_time = (datetime.datetime.now() - start_time).seconds
+        print("--",operation,f"execution time {delta_time}s")
 
     op.execution_time()
 
