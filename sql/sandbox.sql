57ea86490b83fdf7dcd6d74273c128a10b14ac34
Alexis Perrier <alexis.perrier@pm.me>
2021-02-09 06:34:54 +0100

    sandbox

--- sql/sandbox.sql
+++ sql/sandbox.sql
@@ -1,14 +1,13 @@
--- some queries 
+-- some queries
 
-select count(*)
+select v.video_id, c.*
 from video v
-left join discussions d on d.video_id = v.video_id
+left join comments c  on c.video_id = v.video_id
 where v.channel_id in (
     select distinct(channel_id)
     from collection_items
-    where collection_id in (13, 15, 20,24)
+    where collection_id in (24)
     order by channel_id
-    limit 100 offset 100
+    limit 100
 )
-and v.published_at > now() - interval '12 months'
-and d.id is null;
+and v.published_at > '2020-12-01' limit 10;
